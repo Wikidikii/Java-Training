7dbdbe1b6b4e36a7358e20f68a3d8aa63ee87a00
Kawê Almeida <68926941+Wikidikii@users.noreply.github.com>
2023-03-02 09:26:16 -0300

    Add files via upload

---
+++ Ç.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ç;
+
+/**
+ *
+ * @author kawe.silva_uscsonline
+ */
+public class Ç {
+
+
+    public static void main(String[] args) {
+        nsg();
+        nsg("Maneiro!!", "");
+        nsg("Adoro hertistone", "");
+        nsg("Rap do", " Minecraft");
+    }
+      
+    
+     public static void nsg() {
+     String msg = "Matematica wow";
+      
+      System.out.println(msg.toUpperCase());
+     }
+     public static void nsg(String m, String n) {
+         System.out.println(m+n);
+     }
+     }
